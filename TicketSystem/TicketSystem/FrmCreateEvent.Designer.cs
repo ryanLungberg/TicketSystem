e67a6a455f4d4208c492de7775a33f0233e3de4c
dbasargin <dennisbasargin@hotmail.com>
2017-01-18 17:41:48 -0800

    Created empty forms for home, create Performer, create event, and Create category.

---
+++ TicketSystem/TicketSystem/FrmCreateEvent.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace TicketSystem
+{
+    partial class FrmCreateEvent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmCreateEvent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 670);
+            this.Name = "FrmCreateEvent";
+            this.Text = "FrmCreateEvent";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
