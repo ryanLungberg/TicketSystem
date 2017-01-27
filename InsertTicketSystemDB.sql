24dc5bb1fa14d72ed5973e80fd28dc0e95a75ad8
Derek McFarland <mcfarland.derek@gmail.com>
2017-01-26 18:57:01 -0800

    Finished With the filler data
    I have most of the filler data inserted however I'm Having trouble  With date time.  Let me know what you think

--- InsertTicketSystemDB.sql
+++ InsertTicketSystemDB.sql
@@ -12,33 +12,9 @@ INSERT INTO [dbo].[Venues]
            ,[VenueState]
            ,[VenueInfo])
      VALUES
-           (
-		   'Tacoma Dome'
-           ,NULL
-           ,'www.tacomadome.org'
-           ,'2727 East D St'
-           ,'Tacoma'
-           ,'WA'
-           ,'TODO: FILL IN LATER'
-		   ),
-		   (
-		   'Key Arena'
-           ,NULL
-           ,'key.arenaseattle.com/'
-           ,'1st Avenue N and 305 Harrison Street'
-           ,'Seattle'
-           ,'WA'
-           ,'TODO: FILL IN LATER'
-		   ),
-		   (
-		   'Paramount Theatre'
-           ,NULL
-           ,'paramount.theater-seattle.com'
-           ,'911 Pine St'
-           ,'Seattle'
-           ,'WA'
-           ,'TODO: FILL IN LATER'
-		   )
+           ('Tacoma Dome',NULL ,'www.tacomadome.org' ,'2727 East D St' ,'Tacoma' ,'WA' ,'TODO: FILL IN LATER'  ),
+		   ('Key Arena',NULL ,'key.arenaseattle.com/' ,'1st Avenue N and 305 Harrison Street' ,'Seattle' ,'WA' ,'TODO: FILL IN LATER' ),
+		   ('Paramount Theatre' ,NULL ,'paramount.theater-seattle.com' ,'911 Pine St' ,'Seattle' ,'WA' ,'TODO: FILL IN LATER' )
 GO
 USE [TicketSystemDB]
 GO
@@ -58,9 +34,10 @@ INSERT INTO [dbo].[Performer]
            ,[PerformerURL]
            ,[PerformerInfo])
      VALUES
-           (<PerformerName, varchar(50),>
-           ,<PerformerURL, varchar(50),>
-           ,<PerformerInfo, varchar(max),>)
+           ('Burno Mars','www.brunomars.com/','TODO'),
+		   ('WWE', 'www.wwe.com', 'TODO'),
+		   ('Sarah Silverman', 'sarahsilvermanonline.com', 'TODO')
+
 GO
 
 
@@ -68,23 +45,33 @@ GO
 USE [TicketSystemDB]
 GO
 
+ALTER TABLE Events 	ADD EventEndDate	Datetime	NOT NULL;
+EXEC sp_rename 'Events.EventDates', 'EventStartDate';
+
+
+USE [TicketSystemDB]
+GO
+
 INSERT INTO [dbo].[Events]
            ([EventName]
-           ,[EventDates]
+           ,[EventStartDate]
            ,[EventPriceRange]
            ,[EventPromoter]
            ,[EventInfo]
            ,[VenueID]
            ,[GenreID]
-           ,[PerformerID])
+           ,[PerformerID]
+           ,[EventEndDate])
      VALUES
-           (<EventName, varchar(50),>
-           ,<EventDates, datetime,>
-           ,<EventPriceRange, varchar(50),>
-           ,<EventPromoter, varchar(50),>
-           ,<EventInfo, varchar(max),>
-           ,<VenueID, int,>
-           ,<GenreID, int,>
-           ,<PerformerID, int,>)
+           ('Bruno Mars: 24K Magic World Tour','Jul-24-2017' ,'$24.00 - $200.00' ,null ,'TODO'
+           ,1 ,1 ,1 , 'Jul-24-2017')
+		   
+		   ,('WWE SmackDown','Feb-7-2017' ,'$50.00 - $250.00' ,'WWE' ,'TODO'
+           ,2 ,2 ,2 ,'Feb-7-2017')
+
+		   ,('Sarah Silverman Standup' ,'Feb-7-2017' ,'$25.00- $75.00' ,NULL ,'TODO'
+           ,3 ,3 ,3 ,'Feb-7-2017')
 GO
 
+SELECT *
+FROM Venues
