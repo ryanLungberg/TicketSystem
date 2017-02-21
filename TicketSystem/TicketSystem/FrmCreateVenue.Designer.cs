119fe8f17c8dcdb63ecee558b14f7a99c06a3659
dmcfarland <dmcfarland@B11-R111E-05>
2017-02-21 14:24:13 -0800

    Got Code working for Create Event

--- TicketSystem/TicketSystem/FrmCreateVenue.Designer.cs
+++ TicketSystem/TicketSystem/FrmCreateVenue.Designer.cs
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
             this.lblName = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -47,60 +47,60 @@
             this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(222, 88);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(538, 20);
-            this.textBox1.TabIndex = 0;
+            this.txtName.Location = new System.Drawing.Point(221, 42);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(538, 20);
+            this.txtName.TabIndex = 0;
             // 
-            // textBox2
+            // txtType
             // 
-            this.textBox2.Location = new System.Drawing.Point(222, 182);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(538, 20);
-            this.textBox2.TabIndex = 1;
+            this.txtType.Location = new System.Drawing.Point(221, 80);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(538, 20);
+            this.txtType.TabIndex = 1;
             // 
-            // textBox3
+            // txtURL
             // 
-            this.textBox3.Location = new System.Drawing.Point(222, 225);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(538, 20);
-            this.textBox3.TabIndex = 2;
+            this.txtURL.Location = new System.Drawing.Point(221, 123);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(538, 20);
+            this.txtURL.TabIndex = 2;
             // 
-            // textBox4
+            // txtAddress
             // 
-            this.textBox4.Location = new System.Drawing.Point(222, 287);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(538, 20);
-            this.textBox4.TabIndex = 3;
+            this.txtAddress.Location = new System.Drawing.Point(221, 185);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(538, 20);
+            this.txtAddress.TabIndex = 3;
             // 
-            // textBox5
+            // txtCity
             // 
-            this.textBox5.Location = new System.Drawing.Point(222, 349);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(538, 20);
-            this.textBox5.TabIndex = 4;
+            this.txtCity.Location = new System.Drawing.Point(221, 247);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(538, 20);
+            this.txtCity.TabIndex = 4;
             // 
-            // textBox6
+            // txtState
             // 
-            this.textBox6.Location = new System.Drawing.Point(222, 411);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(538, 20);
-            this.textBox6.TabIndex = 5;
+            this.txtState.Location = new System.Drawing.Point(221, 309);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(538, 20);
+            this.txtState.TabIndex = 5;
             // 
-            // textBox7
+            // txtInfo
             // 
-            this.textBox7.Location = new System.Drawing.Point(222, 473);
-            this.textBox7.Multiline = true;
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(538, 127);
-            this.textBox7.TabIndex = 6;
+            this.txtInfo.Location = new System.Drawing.Point(221, 371);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(538, 127);
+            this.txtInfo.TabIndex = 6;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(123, 95);
+            this.lblName.Location = new System.Drawing.Point(122, 49);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(66, 13);
             this.lblName.TabIndex = 7;
@@ -109,7 +109,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(121, 189);
+            this.label2.Location = new System.Drawing.Point(120, 87);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 8;
@@ -118,7 +118,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(123, 232);
+            this.label3.Location = new System.Drawing.Point(122, 130);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 9;
@@ -127,7 +127,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(107, 294);
+            this.label4.Location = new System.Drawing.Point(106, 192);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 13);
             this.label4.TabIndex = 10;
@@ -136,7 +136,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(128, 356);
+            this.label5.Location = new System.Drawing.Point(127, 254);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(61, 13);
             this.label5.TabIndex = 11;
@@ -145,7 +145,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(120, 418);
+            this.label6.Location = new System.Drawing.Point(119, 316);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(69, 13);
             this.label6.TabIndex = 12;
@@ -154,7 +154,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(90, 480);
+            this.label7.Location = new System.Drawing.Point(89, 378);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(99, 13);
             this.label7.TabIndex = 13;
@@ -163,7 +163,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(390, 55);
+            this.label1.Location = new System.Drawing.Point(389, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(106, 13);
             this.label1.TabIndex = 14;
@@ -171,16 +171,17 @@
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(605, 694);
+            this.btnClear.Location = new System.Drawing.Point(604, 553);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(155, 61);
             this.btnClear.TabIndex = 15;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(222, 694);
+            this.btnSubmit.Location = new System.Drawing.Point(221, 553);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(172, 61);
             this.btnSubmit.TabIndex = 16;
@@ -192,7 +193,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 767);
+            this.ClientSize = new System.Drawing.Size(934, 824);
             this.ControlBox = false;
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.btnClear);
@@ -204,16 +205,17 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblName);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.txtName);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmCreateVenue";
             this.Text = "Create A Venue";
+            this.Load += new System.EventHandler(this.FrmCreateVenue_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,13 +223,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtType;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtInfo;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
--- TicketSystem/TicketSystem/FrmCreateVenue.cs
+++ TicketSystem/TicketSystem/FrmCreateVenue.cs
@@ -16,12 +16,62 @@ namespace TicketSystem
         {
             InitializeComponent();
         }
+        TicketSystemDBEntities db = new TicketSystemDBEntities();
 
+        /// <summary>
+        /// Will Add All information from textboxes to TicketSystemDB -Table- Venue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnSubmit_Click(object sender, EventArgs e)
         {
+            Venue v = new Venue();
+
+            v.VenueName = txtName.Text;
+            v.VenueType = txtType.Text;
+            v.VenueAddress = txtAddress.Text;
+            v.VenueCity = txtCity.Text;
+            v.VenueInfo = txtInfo.Text;
+            v.VenueState = txtState.Text;
+            v.VenueURL = txtURL.Text;
+
+            db.Venues.Add(v);
+
+            int insert = db.SaveChanges();
+            if (insert > 0)
+            {
+                MessageBox.Show("Venue Submitted Correctly");
+                
+                
+            }
+            else
+            {
+                MessageBox.Show("Try Again!!!");
+            }
+            
             
         }
 
-        
+
+        /// <summary>
+        /// Once button is pressed all information in text boxes will be cleared
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (Control c in Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+            }
+        }
+
+        private void FrmCreateVenue_Load(object sender, EventArgs e)
+        {
+            Dock = DockStyle.Fill;
+        }
     }
 }
