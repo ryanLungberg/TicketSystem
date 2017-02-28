842578726252730250d51787cec170781296c4f9
dmcfarland <dmcfarland@B11-R111E-05>
2017-02-28 15:04:20 -0800

    started working on create event and Venue ran into some problems it will hot saave to database however will continue to work

--- TicketSystem/TicketSystem/FrmCreateEvent.Designer.cs
+++ TicketSystem/TicketSystem/FrmCreateEvent.Designer.cs
@@ -28,7 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cboGenre = new System.Windows.Forms.ComboBox();
+            this.cboVenue = new System.Windows.Forms.ComboBox();
+            this.cboPerformer = new System.Windows.Forms.ComboBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtPromoter = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // cboGenre
+            // 
+            this.cboGenre.FormattingEnabled = true;
+            this.cboGenre.Location = new System.Drawing.Point(273, 357);
+            this.cboGenre.Name = "cboGenre";
+            this.cboGenre.Size = new System.Drawing.Size(272, 21);
+            this.cboGenre.TabIndex = 0;
+            // 
+            // cboVenue
+            // 
+            this.cboVenue.FormattingEnabled = true;
+            this.cboVenue.Location = new System.Drawing.Point(273, 407);
+            this.cboVenue.Name = "cboVenue";
+            this.cboVenue.Size = new System.Drawing.Size(272, 21);
+            this.cboVenue.TabIndex = 1;
+            // 
+            // cboPerformer
+            // 
+            this.cboPerformer.FormattingEnabled = true;
+            this.cboPerformer.Location = new System.Drawing.Point(273, 457);
+            this.cboPerformer.Name = "cboPerformer";
+            this.cboPerformer.Size = new System.Drawing.Size(272, 21);
+            this.cboPerformer.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(273, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(272, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(273, 210);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(272, 20);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // txtPromoter
+            // 
+            this.txtPromoter.Location = new System.Drawing.Point(273, 259);
+            this.txtPromoter.Name = "txtPromoter";
+            this.txtPromoter.Size = new System.Drawing.Size(272, 20);
+            this.txtPromoter.TabIndex = 7;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(273, 308);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(272, 20);
+            this.txtInfo.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(185, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Event Info:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Event Promoter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(179, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Event Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Event End Date:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 112);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Event Start Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(185, 63);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Event Name:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(185, 365);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Event Genre:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(185, 407);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Event Venue";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(185, 457);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Event perfomer:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(388, 22);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "label10";
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Location = new System.Drawing.Point(273, 112);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(272, 20);
+            this.dtpStartDate.TabIndex = 19;
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.Location = new System.Drawing.Point(273, 161);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(272, 20);
+            this.dtpEndDate.TabIndex = 20;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(707, 161);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 21;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // FrmCreateEvent
             // 
@@ -36,14 +223,56 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(934, 552);
             this.ControlBox = false;
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtPromoter);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.cboPerformer);
+            this.Controls.Add(this.cboVenue);
+            this.Controls.Add(this.cboGenre);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FrmCreateEvent";
             this.Text = "Create An Event";
             this.Load += new System.EventHandler(this.FrmCreateEvent_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cboGenre;
+        private System.Windows.Forms.ComboBox cboVenue;
+        private System.Windows.Forms.ComboBox cboPerformer;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtPromoter;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
--- TicketSystem/TicketSystem/FrmCreateEvent.cs
+++ TicketSystem/TicketSystem/FrmCreateEvent.cs
@@ -15,11 +15,86 @@ namespace TicketSystem
         public FrmCreateEvent()
         {
             InitializeComponent();
+            PopulateGenres();
+            PopulateVenue();
+            PopulatePerformer();
         }
+        TicketSystemDBEntities db = new TicketSystemDBEntities();
 
         private void FrmCreateEvent_Load(object sender, EventArgs e)
         {
             this.Dock = DockStyle.Fill;
         }
+
+
+        public void PopulateGenres()
+        {
+            //TicketSystemDBEntities db = new TicketSystemDBEntities();
+            List<Genre> listGenres = db.Genres.ToList();
+            cboGenre.DataSource = listGenres;
+            cboGenre.DisplayMember = nameof(Genre.GenreName);
+            cboGenre.ValueMember = nameof(Genre.GenreID);
+        }
+
+        public void PopulateVenue()
+        {
+            //TicketSystemDBEntities db = new TicketSystemDBEntities();
+            List<Venue> listGenres = db.Venues.ToList();
+            cboVenue.DataSource = listGenres;
+            cboVenue.DisplayMember = nameof(Venue.VenueName);
+            cboVenue.ValueMember = nameof(Venue.VenueID);
+        }
+
+        public void PopulatePerformer()
+        {
+            //TicketSystemDBEntities db = new TicketSystemDBEntities();
+            List<Performer> listGenres = db.Performers.ToList();
+            cboPerformer.DataSource = listGenres;
+            cboPerformer.DisplayMember = nameof(Performer.PerformerName);
+            cboPerformer.ValueMember = nameof(Performer.PerformerID);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (IsValidData())
+            {
+                Event ev = new Event();
+
+                ev.EventName = txtName.Text;
+                ev.EventPriceRange = txtPrice.Text;
+                ev.EventPromoter = txtPromoter.Text;
+                ev.EventStartDate = dtpStartDate.Value;
+                ev.EventEndDate = dtpEndDate.Value;
+                ev.VenueID = ((Venue)cboVenue.SelectedItem).VenueID;
+                ev.GenreID = cboGenre.SelectedIndex;
+                ev.PerformerID = cboPerformer.SelectedIndex;
+
+                db.Events.Add(ev);
+                int pass = db.SaveChanges();
+                if (pass > 0)
+                {
+                    MessageBox.Show("You have just created a new event", "Yay!!!!");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("You did not enter things in correctly", "Try again");
+            }
+
+        }
+
+        private bool IsValidData()
+        {
+            if (!String.IsNullOrWhiteSpace(txtName.Text))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
